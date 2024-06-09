806e1c891cebdc34539d5026b266118221c86b66
shan19990 <shankhanil.ghosh123@gmail.com>
2024-06-09 14:18:17 +0530

    env

--- dota2boosting/settings.py
+++ dota2boosting/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(8v)&pumhw2fpfbbzhohf8^=+(dg5%%^zr!b-f80)-85aerb-1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
